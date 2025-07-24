aa9ac211078f0a925f9b719e0cddb4ed9574d67b
Dan Schneider <32646250+djschnei21@users.noreply.github.com>
2025-07-24 11:39:20 -0400

    gcp resources

--- main.tf
+++ main.tf
@@ -34,3 +34,31 @@ provider "vault" {
   address = "https://vault-morgan-gcp-demo-public-vault-9febeef3.91f10f5f.z1.hashicorp.cloud:8200"
   namespace = "admin"
 }
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
+resource "google_service_account" "secrets_engine" {
+  account_id   = "hcp-vault-secrets-engine"
+  display_name = "HCP Vault Secrets Engine"
+}
+
+# Updates the IAM policy to grant the service account permissions
+# within the project.
+#
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
+resource "google_project_iam_member" "secrets_engine" {
+  for_each = toset([
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.serviceAccountKeyAdmin",
+    "roles/resourcemanager.projectIamAdmin"
+  ])
+  project = var.gcp_project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.secrets_engine.email}"
+}
+
+# Credentials for HCP Vault to use to authenticate with GCP.
+#
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account_key
+resource "google_service_account_key" "secrets_engine_key" {
+  service_account_id = google_service_account.secrets_engine.name
+}
