de3f1c5788b5342873dc6bb59b38c709e893cc98
Dan Schneider <32646250+djschnei21@users.noreply.github.com>
2025-07-24 11:17:46 -0400

    hcp config

--- main.tf
+++ main.tf
@@ -4,10 +4,18 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.45.0"
     }
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.109.0"
+    }
   }
 }
 
 provider "google" {
   project     = "hc-ec2367a30f42472eb1834003fee"
   region      = "us-east1"
+}
+
+provider "hcp" {
+  project_id = "f1fc24b1-4dbb-44e9-a632-dc130651e59e"
 }
