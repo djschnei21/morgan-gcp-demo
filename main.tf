7610d112517a0475cea779d57b96844da4c559a3
Dan Schneider <32646250+djschnei21@users.noreply.github.com>
2025-07-24 11:03:39 -0400

    tf config

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.45.0"
+    }
+  }
+}
 
 provider "google" {
   project     = "hc-ec2367a30f42472eb1834003fee"
