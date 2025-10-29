d6e30ff6c81f333409a2065362874e4aa81f82cc
Dan Schneider <32646250+djschnei21@users.noreply.github.com>
2025-10-29 12:19:56 -0400

    random name

--- terraform-workspaces/vault-backed/main.tf
+++ terraform-workspaces/vault-backed/main.tf
@@ -3,8 +3,12 @@ provider "google" {
   region      = "us-central1"
 }
 
+resource "random_pet" "name" {
+  length = 4
+}
+
 resource "google_storage_bucket" "no-public-access" {
-  name          = "no-public-access-bucket"
+  name          = "no-public-access-bucket-${random_pet.name.id}"
   location      = "US"
   force_destroy = true
 
